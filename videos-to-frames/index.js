a804fc74162dad2524ed9a3fc68ef840aadf3600
DominicDev <s0l0estudi4r@gmail.com>
2026-02-05 23:13:51 +0100

    v0.1.2

--- videos-to-frames/index.js
+++ videos-to-frames/index.js
@@ -7,11 +7,11 @@ import sharp from 'sharp';
 import { directoryFrames, directoryVideos, GLOBAL_PREFIX_IMAGES_NUM } from '../config.js';
 const PREFIX_NAME_VIDEO = 'v';
 const START_VIDEO = 1;
-const END_VIDEO = 5;
+const END_VIDEO = 7;
 const OUTPUT = directoryFrames;
 const FPS = 1;
 const PREFIX_NUM = GLOBAL_PREFIX_IMAGES_NUM || 0;
-const PREFIX = 'f';
+const PREFIX = 'u';
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 ffmpeg.setFfprobePath(ffprobePath.path);
